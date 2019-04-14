351fa686eb3fbdbc014b7c944350ae5d9b099290
datou <1392767180@qq.com>
2019-04-14 22:00:47 +0800

    无缝轮播

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 1. flex使图片一行
 2. dom.offset()
 3. page visiable
+4. goToSlide分离出来很关键
--- index.js
+++ index.js
@@ -1,36 +1,58 @@
 let img = images.children;
 let lastIndex = 1;  // 上一个索引
+let timer = null;
 // 制作按钮
 addButton();
 // 克隆第一张和最后一张，并插入
 cloneNode();
 // 点哪个动哪个
 listenToClick();
-
-
+// 上一张、下一张
+let btnLen = btnWrapper.children.length;
+next.addEventListener('click', function () {
+  lastIndex++;
+  if (lastIndex > btnLen) {
+    lastIndex = btnLen;
+    goToSlide(1);
+  } else {
+    goToSlide(lastIndex);
+  }
+})
+previous.addEventListener('click', function () {
+  lastIndex--;
+  if (lastIndex < 1) {
+    lastIndex = 1;
+    goToSlide(4);
+  } else {
+    goToSlide(lastIndex);
+  }
+})
 function listenToClick() {
   btnWrapper.addEventListener('click', function (e) {
-    let btnLen = btnWrapper.children.length;
     index = e.target.innerText;
     if (!e.target.tagName === "SPAN") { return; }
-    if (lastIndex == 1 && index == btnLen) {  // 如果上一张是第一张并且当前点的是最后一张
-      to(0);  // 先滑到最左边（克隆的最后一张）
-      images.addEventListener('transitionend', function () {  // 动画结束后马上切到真的最后一张  
-        firstOrEndTo(-400 * btnLen);
-        images.removeEventListener('transitionend', arguments.callee);
-      })
-    } else if (lastIndex == btnLen && index == 1) {  // 如果上一张是最后一张并且当前点的是最后一张
-      to(-400 * (btnLen + 1));  // 先滑到最右边（克隆的第一张）
-      images.addEventListener('transitionend', function () {  // 然后动画结束后马上切到真的第一张
-        firstOrEndTo(-400)
-        images.removeEventListener('transitionend', arguments.callee);
-      })
-    } else {
-      // 正常情况
-      to(-400 * index);
-    }
-    lastIndex = e.target.innerText;
+    goToSlide(index);
   })
+}
+function goToSlide(index) {
+  if (lastIndex == 1 && index == btnLen) {  // 如果上一张是第一张并且当前点的是最后一张
+    to(0);  // 先滑到最左边（克隆的最后一张）
+    images.addEventListener('transitionend', function () {  // 动画结束后马上切到真的最后一张  
+      firstOrEndTo(-400 * btnLen);
+      images.removeEventListener('transitionend', arguments.callee);
+    })
+  } else if (lastIndex == btnLen && index == 1) {  // 如果上一张是最后一张并且当前点的是最后一张
+    console.log('here')
+    to(-400 * (btnLen + 1));  // 先滑到最右边（克隆的第一张）
+    images.addEventListener('transitionend', function () {  // 然后动画结束后马上切到真的第一张
+      firstOrEndTo(-400)
+      images.removeEventListener('transitionend', arguments.callee);
+    })
+  } else {
+    // 正常情况
+    to(-400 * index);
+  }
+  lastIndex = index;
 }
 function to(pos) {
   images.style.transform = `translateX(${pos}px)`;
