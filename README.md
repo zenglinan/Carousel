89f53dad70fed9633479cadc35b449a9315e9749
datou <1392767180@qq.com>
2019-07-08 19:38:09 +0800

    重写轮播

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 #### 要点
-1. flex使图片一行
-2. dom.offset()
-3. page visiable
-4. goToSlide分离出来很关键
+1. index = (index - 1 + dots.length) % dots.length  // 圆点循环算法
+2. 查找元素在同级元素中的位置
+```
+const dots = Array.from($$(".carousel .actions .dots span"))
+let index = dots.indexOf(dot)
+```
--- index.js
+++ index.js
@@ -1,80 +1,43 @@
-let img = images.children;
-let lastIndex = 1;  // 上一个索引
-let timer = null;
-// 制作按钮
-addButton();
-// 克隆第一张和最后一张，并插入
-cloneNode();
-// 点哪个动哪个
-listenToClick();
-// 上一张、下一张
-let btnLen = btnWrapper.children.length;
-next.addEventListener('click', function () {
-  lastIndex++;
-  if (lastIndex > btnLen) {
-    lastIndex = btnLen;
-    goToSlide(1);
-  } else {
-    goToSlide(lastIndex);
-  }
-})
-previous.addEventListener('click', function () {
-  lastIndex--;
-  if (lastIndex < 1) {
-    lastIndex = 1;
-    goToSlide(4);
-  } else {
-    goToSlide(lastIndex);
-  }
-})
-function listenToClick() {
-  btnWrapper.addEventListener('click', function (e) {
-    index = e.target.innerText;
-    if (!e.target.tagName === "SPAN") { return; }
-    goToSlide(index);
-  })
+const $ = s => document.querySelector(s)
+const $$ = s => document.querySelectorAll(s)
+
+// 动态生成圆点
+const images = Array.from($$(".carousel .panels img"))
+const dotsWrapper = $(".carousel .actions .dots")
+for (let i = 0; i < images.length; i++) {
+  let span = document.createElement("span")
+  dotsWrapper.appendChild(span)
 }
-function goToSlide(index) {
-  if (lastIndex == 1 && index == btnLen) {  // 如果上一张是第一张并且当前点的是最后一张
-    to(0);  // 先滑到最左边（克隆的最后一张）
-    images.addEventListener('transitionend', function () {  // 动画结束后马上切到真的最后一张  
-      firstOrEndTo(-400 * btnLen);
-      images.removeEventListener('transitionend', arguments.callee);
-    })
-  } else if (lastIndex == btnLen && index == 1) {  // 如果上一张是最后一张并且当前点的是最后一张
-    console.log('here')
-    to(-400 * (btnLen + 1));  // 先滑到最右边（克隆的第一张）
-    images.addEventListener('transitionend', function () {  // 然后动画结束后马上切到真的第一张
-      firstOrEndTo(-400)
-      images.removeEventListener('transitionend', arguments.callee);
-    })
-  } else {
-    // 正常情况
-    to(-400 * index);
-  }
-  lastIndex = index;
+// 初始化第一张照片和圆点
+$(".carousel .actions .dots span:nth-child(1)").classList.add("active")
+$(".carousel .panels img:nth-child(1)").classList.add("active")
+const dots = Array.from($$(".carousel .actions .dots span")) // 关键！
+
+// 给圆点绑定点击事件
+dotsWrapper.onclick = function(e) {
+  if (e.target.tagName !== 'SPAN') return
+  let index = dots.indexOf(e.target)
+  setDot(index)
+  setPanel(index)
 }
-function to(pos) {
-  images.style.transform = `translateX(${pos}px)`;
+pre.onclick = function(){
+  let index = dots.indexOf($(".carousel .actions .dots span.active"))
+  index = (index - 1 + dots.length) % dots.length  // 关键！
+  setDot(index)
+  setPanel(index)
 }
-function cloneNode() {
-  let firstClone = img[0].cloneNode(true);
-  let lastClone = img[img.length - 1].cloneNode(true);
-  images.appendChild(firstClone);
-  images.prepend(lastClone);
-};
-function firstOrEndTo(pos) {
-  images.style.display = "none";
-  images.getBoundingClientRect();
-  images.style.transform = `translateX(${pos}px)`
-  images.style.display = "flex";
-  images.removeEventListener('transitionend', arguments.callee);
+next.onclick = function(){
+  let index = dots.indexOf($(".carousel .actions .dots span.active"))
+  index = (index + 1 + dots.length) % dots.length
+  setDot(index)
+  setPanel(index)
+  
 }
-function addButton() {
-  for (let i = 1; i <= img.length; i++) {
-    let newBtn = document.createElement('span');
-    let btnNum = document.createTextNode(`${i}`);
-    newBtn.appendChild(btnNum);
-    btnWrapper.appendChild(newBtn);
-  }
+function setDot(index){
+  dots.forEach((dot) => dot.classList.remove("active"))
+  dots[index].classList.add("active")
+}
+function setPanel(index){
+  images.forEach((img) => img.classList.remove("active"))
+  images[index].classList.add("active")
 }
